b06a8cde7008ecade8207a0944c724f7e2a70d93
Erik Clarke <ecl@mail.med.upenn.edu>
2015-01-15 16:57:56 -0500

    bandwidth parameter. also some code cleanup, doc updates

--- R/position-beeswarm.r
+++ R/position-beeswarm.r
@@ -3,46 +3,43 @@
 #' @import proto
 #' @name position-beeswarm
 #' @family position adjustments
-#' @param width degree of jitter in x direction. Defaults to 90\% of the
-#'   resolution of the data.
-#' @param nbins the number of divisions of the y-axis to use (default: \code{length(y)/5})
+#' @param width (currently a no-op)
+#' @param nbins the number of divisions of the y-axis to use (default:
+#'   \code{length(y)/5})
 #' @param spacing the spacing between adjacent dots (default: 1/20)
+#' @param bandwidth the adjustment to the kernel density bandwidth (higher
+#'   values increase the number of points captured in a bin) (default: 1/10)
 #' @export
 #' @examples
-#'
+#' 
 #' qplot(class, hwy, data = mpg, position="beeswarm")
 #' # Generate fake data
 #' distro <- melt(data.frame(list(runif=runif(100, min=-3, max=3), rnorm=rnorm(100))))
 #' qplot(variable, value, data = distro, position = "beeswarm")
-#' # Spacing and y-bin width can be adjusted
+#' # Spacing and number of bins can be adjusted
 #' qplot(variable, value, data = distro, position = position_beeswarm(spacing = 1/20, nbins=35))
-#' # Alternatively, can specify \code{width} parameter like \code{position_jitter}
-#' qplot(variable, value, data = distro, position = position_beeswarm(width=0.4)
 
-position_beeswarm <- function (width = NULL, nbins = NULL, spacing = NULL, sesh=NULL,
-                               sesh2=NULL) {
+position_beeswarm <- function (width = NULL, nbins = NULL, spacing = NULL, 
+                               bandwidth = NULL) {
   PositionBeeswarm$new(width = width, nbins = nbins, spacing = spacing,
-                       sesh=sesh, sesh2=sesh2)
+                       bandwidth = bandwidth)
 }
 
 PositionBeeswarm <- proto(ggplot2:::Position, {
   width = NULL
   nbins = NULL
   spacing = NULL
-  sesh = NULL
-  sesh2 = NULL
+  bandwidth = NULL
 
   new <- function(.,
                   width = NULL,
                   nbins = NULL,
                   spacing = NULL,
-                  sesh=NULL,
-                  sesh2=NULL) {
+                  bandwidth = NULL) {
     .$proto(width=width,
             nbins=nbins,
             spacing=spacing,
-            sesh=sesh,
-            sesh2=sesh2)
+            bandwidth=bandwidth)
   }
 
   objname <- "beeswarm"
@@ -52,14 +49,13 @@ PositionBeeswarm <- proto(ggplot2:::Position, {
     if (empty(data)) return(data.frame())
     check_required_aesthetics(c("x", "y"), names(data), "position_jitter")
 
-    if (is.null(.$width)) .$width <- resolution(data$x, zero = FALSE) * 0.9
+#     if (is.null(.$width)) .$width <- resolution(data$x, zero = FALSE) * 0.9
     if (is.null(.$nbins)) {
-      n <- length(data$y)
-#       .$nbins <- as.integer(length(data$y)/(2*density(data$y)$bw))
       .$nbins <- as.integer(length(data$y)/5)
       message("Default number of y-bins used (", .$nbins, ").")
     }
     if (is.null(.$spacing)) .$spacing <- 1/20
+    if (is.null(.$bandwidth)) .$bandwidth <- 1/10
     trans_x <- NULL
     trans_y <- NULL
 
@@ -72,15 +68,12 @@ PositionBeeswarm <- proto(ggplot2:::Position, {
 
 
       trans_x <- function(x) {
-#         dens <- density(data$y, adjust=.$sesh)
-#         y_auc <- cumsum(((.$sesh2-dens$y)^(.$sesh))*diff(dens$x)[1])
-#         y_cuts <- cut(y_auc, .$nbins)
-#         y_bins <- sapply(split(dens$x, y_cuts), min)
         split_y <- split(data$y, x)
+        ## TODO: re-enable width param
         max_len <- NULL #max(sapply(split_y, function(i) max(table(cut(i, y_bins)))))
 
         x_offsets <- lapply(split_y, function(x_class) {
-          dens <- density(x_class, adjust=1/10)
+          dens <- density(x_class, adjust=.$bandwidth)
           y_auc <- cumsum((((1+max(dens$y))-dens$y)^(1/3))*diff(dens$x)[1])
           y_cuts <- cut(y_auc, .$nbins)
           y_bins <- sapply(split(dens$x, y_cuts), min)
