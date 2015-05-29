4581f2e787df40ca34eeaf4013fb3f7ce185bc67
Erik Clarke <ecl@mail.med.upenn.edu>
2015-05-29 16:07:28 -0400

    Updated docs/readme and examples

--- R/ggbeeswarm.R
+++ R/ggbeeswarm.R
@@ -7,15 +7,31 @@
 #' plots. Returns a vector of x-offsets of the same length that 
 #' 
 #' @param y vector of data points 
-#' @param x a grouping factor for y (i.e. what will be the x axis)
+#' @param x a grouping factor for y (optional)
 #' @param width the maximum x-width of each group (0 = no width, 1 = max; default = 0.4)
 #' @param var_width adjust the width of each group based on the number of points in the group
 #' @param adjust bandwidth used to adjust the density
 #' @param nbins the number of points used to calculate density
 #' @export
 #' @examples 
+#' ## Generate fake data
+#' dat <- list(rnorm(50), rnorm(500), c(rnorm(100), rnorm(100,5)), rcauchy(100))
+#' labels <- c("Normal", "Dense Norm.", "Bimodal", "Extremes")
 #' 
-#' 
+#' ## Plot each distribution with a variety of parameters
+#' par(mfrow=c(4,1), mar=c(2,4, 0.5, 0.5))
+#' mapply(function(y, label) {
+#'   ids <- rep(1:4, each=length(y))
+#'   
+#'   offsets <- c(
+#'     offset_x(y),  # Default
+#'     offset_x(y, adjust=2),    # More smoothing
+#'     offset_x(y, adjust=0.1),  # Tighter fit
+#'     offset_x(y, width=0.1))   # Less wide
+#'   
+#'   plot(offsets + ids, rep(y, 4), ylab=label, xlab='', xaxt='n', pch=21, las=1)
+#'   axis(1, 1:4, c("Default", "Adjust=2", "Adjust=0.1", "Width=10%"))
+#' }, dat, labels)
 #' 
 offset_x <- function(y, x, width=0.4, var_width=FALSE, adjust=0.5, nbins=1000) {
   
@@ -28,7 +44,7 @@ offset_x <- function(y, x, width=0.4, var_width=FALSE, adjust=0.5, nbins=1000) {
   x_offsets <- lapply(y_split, function(y_subgroup) {
     
     # If there's only one value in this group, leave it alone
-    if (length(y_split) == 1) return(0) 
+    if (length(y_subgroup) == 1) return(0) 
     
     out <- rep(NA, length(y_subgroup))
     
