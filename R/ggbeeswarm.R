ed73cac4102d68aa9d157361f71d0fbbc0725bc3
sherrillmix <sherrillmix@users.noreply.github.com>
2015-06-04 16:01:53 -0400

    Clean up spreading function. Also test git

--- R/ggbeeswarm.R
+++ R/ggbeeswarm.R
@@ -34,11 +34,10 @@ offset_x <- function(y, x, width=0.4, var_width=FALSE, adjust=0.5, nbins=1000) {
   
   if (missing(x)) x <- rep(1, length(y))
   
-  # Split the y-values by discrete values of x
-  y_split <- split(y, x)
-  
+  maxLength<-max(table(x))
+
   # Apply the van der Corput noise to each x group to create offsets
-  x_offsets <- lapply(y_split, function(y_subgroup) {
+  new_x <- ave(y,x, FUN=function(y_subgroup) {
     
     # If there's only one value in this group, leave it alone
     if (length(y_subgroup) == 1) return(0) 
@@ -46,22 +45,19 @@ offset_x <- function(y, x, width=0.4, var_width=FALSE, adjust=0.5, nbins=1000) {
     out <- rep(NA, length(y_subgroup))
     
     subgroup_width <- 1
-    if (var_width) subgroup_width <- length(y_subgroup)/max(table(x))
+    if (var_width) subgroup_width <- length(y_subgroup)/maxLength
     
-    dens <- density(y_subgroup, n = nbins, adjust = adjust)
+    dens <- stats::density(y_subgroup, n = nbins, adjust = adjust)
     dens$y <- dens$y / max(dens$y)
     offset <- vanDerCorput(length(y_subgroup))[rank(y_subgroup, ties.method="first")]
+
+    pointDensities<-approx(dens$x,dens$y,y_subgroup)$y
+
+    out<-(offset*width*2-width)*pointDensities*subgroup_width
     
-    for (i in 1:nbins) {
-      # Select the points in a bin
-      selector <- y_subgroup <= dens$x[i] & is.na(out)
-      out[selector] <- (offset[selector] * width * 2 - width) * dens$y[i] * subgroup_width
-    }
     return(out)
   })
   
-  # Join the x-offsets together
-  new_x <- unsplit(x_offsets, x)
   return(new_x)
 }
 
