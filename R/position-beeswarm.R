f8af9a21cec29c2c50f2746aa21ff6bae460c8fb
sherrillmix <sherrillmix@users.noreply.github.com>
2015-10-17 08:22:01 -0400

    Add options to position-beeswarm

--- R/position-beeswarm.R
+++ R/position-beeswarm.R
@@ -1,14 +1,13 @@
 #' Violin point-style plots to show overlapping points. x must be discrete.
 #' 
 #' @family position adjustments
-#' @param width the maximum amount of spread (default: 0.4)
-#' @param varwidth vary the width by the relative size of each group
-#' @param bandwidth the bandwidth adjustment to use when calculating density 
-#' Smaller numbers (< 1) produce a tighter "fit". (default: 0.5)
+#' @param priority Method used to perform point layout (see \code{\link{swarmx}})
+#' @param cex Scaling for adjusting point spacing (see \code{\link{swarmx}})
 #' @export
 #' @import proto
 #' @import ggplot2
 #' @import beeswarm
+#' @seealso \code{\link{swarmx}}
 #' @examples
 #' 
 #'   ggplot2::qplot(class, hwy, data = ggplot2::mpg, position=position_beeswarm())
@@ -18,17 +17,18 @@
 #'     'value'=c(runif(100, min=-3, max=3), rnorm(100))
 #'   )
 #'   ggplot2::qplot(variable, value, data = distro, position = position_beeswarm())
-#'   ggplot2::qplot(variable, value, data = distro, position = position_beeswarm(width=0.1))
+#'   ggplot2::qplot(variable, value, data = distro, position = 
+#'                  position_beeswarm(priority='density'),cex=2.5)
 #'
-position_beeswarm <- function (width = NULL, varwidth = NULL, bandwidth=NULL) {
-	PositionBeeswarm$new(width = width, varwidth = varwidth, bandwidth=bandwidth)
+position_beeswarm <- function (priority = c("ascending", "descending", "density", "random", "none"),cex=2) {
+	PositionBeeswarm$new(priority = priority,cex=cex)
 }
 
 PositionBeeswarm <- proto::proto(ggplot2:::Position, {
 	objname <- "quasirandom"
 
-	new <- function(., width=NULL, varwidth=NULL, bandwidth=NULL) {
-		.$proto(width=width, varwidth=varwidth, bandwidth=bandwidth)
+	new <- function(., priority=c("ascending", "descending", "density", "random", "none"),cex=2) {
+		.$proto(priority=priority,cex=cex)
 	}
 
 	# Adjust function is used to calculate new positions (from ggplot2:::Position)
@@ -36,35 +36,27 @@ PositionBeeswarm <- proto::proto(ggplot2:::Position, {
 		if (empty(data)) return(data.frame())
 		check_required_aesthetics(c("x", "y"), names(data), "position_beeswarm")
 
-		if (is.null(.$width)) .$width <- resolution(data$x, zero = FALSE) * 0.4
-		if (is.null(.$varwidth)) .$varwidth <- FALSE
-		if (is.null(.$bandwidth)) .$bandwidth <- 0.5
-
 		.$newXY<- NULL
 
-		trans_x <- NULL
-		trans_y <- NULL
+		setNewXY<-function(){
+			if(!is.null(.$newXY))return(NULL)
+			#need to replace cex and xsize and ysize with options from ggplot 
+			.$newXY<-do.call(rbind,ave(data$y,data$x,FUN=function(yy)split(beeswarm::swarmx(0,yy,cex=.$cex,priority=.$priority),1:length(yy))))
+			.$newXY$x<-.$newXY$x+data$x
+			.$newXY$oldX<-data$x
+			.$newXY$oldY<-data$y
+			return(NULL)
+		}
 
-		if(.$width > 0) {
-			setNewXY<-function(){
-				if(!is.null(.$newXY))return(NULL)
-				.$newXY<-do.call(rbind,ave(data$y,data$x,FUN=function(yy)split(beeswarm::swarmx(0,yy),1:length(yy))))
-				.$newXY$x<-.$newXY$x+data$x
-				.$newXY$oldX<-data$x
-				.$newXY$oldY<-data$y
-				return(NULL)
-			}
-
-			trans_x <- function(x) {
-				setNewXY()
-				if(any(.$newXY$oldX!=x))stop(simpleError('Mismatch between expected x and x in position'))
-				.$newXY$x
-			}
-			trans_y <- function(y) {
-				setNewXY()
-				if(any(.$newXY$oldY!=y))stop(simpleError('Mismatch between expected y and y in position'))
-				.$newXY$y
-			}
+		trans_x <- function(x) {
+			setNewXY()
+			if(any(.$newXY$oldX!=x))stop(simpleError('Mismatch between expected x and x in position'))
+			.$newXY$x
+		}
+		trans_y <- function(y) {
+			setNewXY()
+			if(any(.$newXY$oldY!=y))stop(simpleError('Mismatch between expected y and y in position'))
+			.$newXY$y
 		}
 
 		transform_position(data, trans_x, trans_y)
