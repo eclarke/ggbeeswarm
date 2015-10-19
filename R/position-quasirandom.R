c760d31abf901cde69077722c8e95b21b7e60034
sherrillmix <sherrillmix@users.noreply.github.com>
2015-10-19 14:09:08 -0400

    Indent left one

--- R/position-quasirandom.R
+++ R/position-quasirandom.R
@@ -22,45 +22,45 @@
 #'   ggplot2::qplot(variable, value, data = distro, position = position_quasirandom())
 #'   ggplot2::qplot(variable, value, data = distro, position = position_quasirandom(width=0.1))
 #'
-	position_quasirandom <- function (width = NULL, varwidth = FALSE, bandwidth=.5,nbins=1000,method='quasirandom') {
-	  PositionQuasirandom$new(width = width, varwidth = varwidth, bandwidth=bandwidth,nbins=nbins,method=method)
-	}
+position_quasirandom <- function (width = NULL, varwidth = FALSE, bandwidth=.5,nbins=1000,method='quasirandom') {
+  PositionQuasirandom$new(width = width, varwidth = varwidth, bandwidth=bandwidth,nbins=nbins,method=method)
+}
 
-	PositionQuasirandom <- proto::proto(ggplot2:::Position, {
-	  objname <- "quasirandom"
-	  
-	  new <- function(., width=NULL, varwidth=FALSE, bandwidth=.5,nbins=1000,method='quasirandom') {
-		 .$proto(width=width, varwidth=varwidth, bandwidth=bandwidth,nbins=nbins,method=method)
-	  }
+PositionQuasirandom <- proto::proto(ggplot2:::Position, {
+  objname <- "quasirandom"
+  
+  new <- function(., width=NULL, varwidth=FALSE, bandwidth=.5,nbins=1000,method='quasirandom') {
+	 .$proto(width=width, varwidth=varwidth, bandwidth=bandwidth,nbins=nbins,method=method)
+  }
 
-	  # Adjust function is used to calculate new positions (from ggplot2:::Position)
-	  adjust <- function(., data) {
-		 if (empty(data)) return(data.frame())
-		 check_required_aesthetics(c("x", "y"), names(data), "position_quasirandom")
+  # Adjust function is used to calculate new positions (from ggplot2:::Position)
+  adjust <- function(., data) {
+	 if (empty(data)) return(data.frame())
+	 check_required_aesthetics(c("x", "y"), names(data), "position_quasirandom")
 
-		 if (is.null(.$width)) .$width <- ggplot2::resolution(data$x, zero = FALSE) * 0.4
-		 
-		 trans_x <- NULL
-		 trans_y <- NULL
-		 
-		 if(.$width > 0) {
-			trans_x <- function(x) {
-			  new_x <- violinPointR::offsetX( #if change the package name then will need to change here
-				 data$y,
-				 x,
-				 width=.$width, 
-				 varwidth=.$varwidth, 
-				 adjust=.$bandwidth,
-				 method=.$method
-			  )
-			  
-			  new_x + x
-			}
-			  
-		 }
+	 if (is.null(.$width)) .$width <- ggplot2::resolution(data$x, zero = FALSE) * 0.4
+	 
+	 trans_x <- NULL
+	 trans_y <- NULL
+	 
+	 if(.$width > 0) {
+		trans_x <- function(x) {
+		  new_x <- violinPointR::offsetX( #if change the package name then will need to change here
+			 data$y,
+			 x,
+			 width=.$width, 
+			 varwidth=.$varwidth, 
+			 adjust=.$bandwidth,
+			 method=.$method
+		  )
+		  
+		  new_x + x
+		}
+		  
+	 }
 
-		 transform_position(data, trans_x, trans_y)
-	  }
+	 transform_position(data, trans_x, trans_y)
+  }
 
-	})
+})
 
