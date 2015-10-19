479b1f74d1dd0f427be4fb87fe4037931f9c3212
sherrillmix <sherrillmix@users.noreply.github.com>
2015-10-19 14:08:37 -0400

    Add dependency on violinPointR (package name may need changed)

--- R/position-quasirandom.R
+++ R/position-quasirandom.R
@@ -5,9 +5,12 @@
 #' @param varwidth vary the width by the relative size of each group
 #' @param bandwidth the bandwidth adjustment to use when calculating density 
 #' Smaller numbers (< 1) produce a tighter "fit". (default: 0.5)
+#' @param nbins the number of bins used when calculating density (has little effect with quasirandom/random distribution)
+#' @param method the method used for distributing points (quasirandom, pseudorandom, smiley or frowney)
 #' @export
 #' @import proto
 #' @import ggplot2
+#' @import violinPointR
 #' @examples
 #' 
 #'   ggplot2::qplot(class, hwy, data = ggplot2::mpg, position=position_quasirandom())
@@ -19,15 +22,15 @@
 #'   ggplot2::qplot(variable, value, data = distro, position = position_quasirandom())
 #'   ggplot2::qplot(variable, value, data = distro, position = position_quasirandom(width=0.1))
 #'
-	position_quasirandom <- function (width = NULL, varwidth = NULL, bandwidth=NULL) {
-	  PositionQuasirandom$new(width = width, varwidth = varwidth, bandwidth=bandwidth)
+	position_quasirandom <- function (width = NULL, varwidth = FALSE, bandwidth=.5,nbins=1000,method='quasirandom') {
+	  PositionQuasirandom$new(width = width, varwidth = varwidth, bandwidth=bandwidth,nbins=nbins,method=method)
 	}
 
 	PositionQuasirandom <- proto::proto(ggplot2:::Position, {
 	  objname <- "quasirandom"
 	  
-	  new <- function(., width=NULL, varwidth=NULL, bandwidth=NULL) {
-		 .$proto(width=width, varwidth=varwidth, bandwidth=bandwidth)
+	  new <- function(., width=NULL, varwidth=FALSE, bandwidth=.5,nbins=1000,method='quasirandom') {
+		 .$proto(width=width, varwidth=varwidth, bandwidth=bandwidth,nbins=nbins,method=method)
 	  }
 
 	  # Adjust function is used to calculate new positions (from ggplot2:::Position)
@@ -35,22 +38,21 @@
 		 if (empty(data)) return(data.frame())
 		 check_required_aesthetics(c("x", "y"), names(data), "position_quasirandom")
 
-		 if (is.null(.$width)) .$width <- resolution(data$x, zero = FALSE) * 0.4
-		 if (is.null(.$varwidth)) .$varwidth <- FALSE
-		 if (is.null(.$bandwidth)) .$bandwidth <- 0.5
-		 
-		 nbins <- 1000
+		 if (is.null(.$width)) .$width <- ggplot2::resolution(data$x, zero = FALSE) * 0.4
 		 
 		 trans_x <- NULL
 		 trans_y <- NULL
 		 
 		 if(.$width > 0) {
 			trans_x <- function(x) {
-			  new_x <- offset_x(
-				 data$y, x,
+			  new_x <- violinPointR::offsetX( #if change the package name then will need to change here
+				 data$y,
+				 x,
 				 width=.$width, 
 				 varwidth=.$varwidth, 
-				 adjust=.$bandwidth)
+				 adjust=.$bandwidth,
+				 method=.$method
+			  )
 			  
 			  new_x + x
 			}
