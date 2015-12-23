ce1f3d174fa3ddfe62cd4a8da392350f482596ca
sherrillmix <sherrillmix@users.noreply.github.com>
2015-12-23 16:04:30 -0500

    Basic geom documentation. Checks without error

--- R/geom-beeswarm.R
+++ R/geom-beeswarm.R
@@ -1,5 +1,19 @@
-
-
+#' Points, jittered to reduce overplotting using the beeswarm package
+#'
+#' The beeswarm geom is a convenient means to offset points within categories to reduce overplotting. Uses the beeswarm package
+#'
+#' @section Aesthetics:
+#' \Sexpr[results=rd,stage=build]{ggplot2:::rd_aesthetics("geom", "point")}
+#'
+#' @inheritParams ggplot2::geom_point
+#' @inheritParams position_beeswarm
+#' @seealso
+#'  \code{\link{geom_quasirandom}} an alternative method,
+#'  \code{\link[beeswarm]{swarmx}} how spacing is determined,
+#'  \code{\link[ggplot2]{geom_point}} for regular, unjittered points,
+#'  \code{\link[ggplot2]{geom_jitter}} for jittered points,
+#'  \code{\link[ggplot2]{geom_boxplot}} for another way of looking at the conditional
+#'     distribution of a variable
 #' @export
 geom_beeswarm <- function(mapping = NULL, data = NULL,
   priority = c("ascending", "descending", "density", "random", "none"),cex=2,groupOnX=NULL,
--- R/geom-quasirandom.R
+++ R/geom-quasirandom.R
@@ -1,5 +1,18 @@
-
-
+#' Points, jittered to reduce overplotting using the vipor package
+#'
+#' The quasirandom geom is a convenient means to offset points within categories to reduce overplotting. Uses the vipor package
+#'
+#' @section Aesthetics:
+#' \Sexpr[results=rd,stage=build]{ggplot2:::rd_aesthetics("geom", "point")}
+#'
+#' @inheritParams ggplot2::geom_point
+#' @inheritParams position_quasirandom
+#' @seealso
+#'  \code{\link[vipor]{offsetX}} how spacing is determined,
+#'  \code{\link[ggplot2]{geom_point}} for regular, unjittered points,
+#'  \code{\link[ggplot2]{geom_jitter}} for jittered points,
+#'  \code{\link{geom_boxplot}} for another way of looking at the conditional
+#'     distribution of a variable
 #' @export
 geom_quasirandom <- function(mapping = NULL, data = NULL,
   width = NULL, varwidth = FALSE, bandwidth=.5,nbins=1000,method='quasirandom',groupOnX=NULL,
