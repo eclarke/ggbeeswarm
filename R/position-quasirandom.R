5a2f6b029c2d5c02dad7f741d1984e6b11056479
sherrillmix <sherrillmix@users.noreply.github.com>
2015-12-23 13:49:46 -0500

    Fix indentation

--- R/position-quasirandom.R
+++ R/position-quasirandom.R
@@ -28,53 +28,53 @@ position_quasirandom <- function (width = NULL, varwidth = FALSE, bandwidth=.5,n
 
 PositionBeeswarm <- ggproto("PositionBeeswarm",ggplot2:::Position, required_aes=c('x','y'),
   compute_layer=function(data,params,panel){
-	# Adjust function is used to calculate new positions (from ggplot2:::Position)
+    # Adjust function is used to calculate new positions (from ggplot2:::Position)
 
-		# more unique entries in x than y suggests y (not x) is categorical
-		if(length(unique(data$y)) < length(unique(data$x))) {
-			swap_xy<-TRUE
-		} else {
-			swap_xy<-FALSE
-		}
+    # more unique entries in x than y suggests y (not x) is categorical
+    if(length(unique(data$y)) < length(unique(data$x))) {
+      swap_xy<-TRUE
+    } else {
+      swap_xy<-FALSE
+    }
 
-		.$newXY<- NULL
+    .$newXY<- NULL
 
-		setNewXY<-function(){
-			if(!is.null(.$newXY))return(NULL)
-			#need to replace cex and xsize and ysize with options from ggplot
-			if(swap_xy) {
-				.$newXY<-do.call(rbind,ave(data$x,data$y,FUN=function(xx)split(beeswarm::swarmy(xx,0,cex=.$cex,priority=.$priority),1:length(xx)))) 
-				.$newXY$y<-.$newXY$y+data$y
-				.$newXY$oldX<-data$x
-				.$newXY$oldY<-data$y
-			} else {
-				.$newXY<-do.call(rbind,ave(data$y,data$x,FUN=function(yy)split(beeswarm::swarmx(0,yy,cex=.$cex,priority=.$priority),1:length(yy)))) 
-				.$newXY$x<-.$newXY$x+data$x
-				.$newXY$oldX<-data$x
-				.$newXY$oldY<-data$y
-			}
-			return(NULL)
-		}
+    setNewXY<-function(){
+      if(!is.null(.$newXY))return(NULL)
+      #need to replace cex and xsize and ysize with options from ggplot
+      if(swap_xy) {
+        .$newXY<-do.call(rbind,ave(data$x,data$y,FUN=function(xx)split(beeswarm::swarmy(xx,0,cex=.$cex,priority=.$priority),1:length(xx)))) 
+        .$newXY$y<-.$newXY$y+data$y
+        .$newXY$oldX<-data$x
+        .$newXY$oldY<-data$y
+      } else {
+        .$newXY<-do.call(rbind,ave(data$y,data$x,FUN=function(yy)split(beeswarm::swarmx(0,yy,cex=.$cex,priority=.$priority),1:length(yy)))) 
+        .$newXY$x<-.$newXY$x+data$x
+        .$newXY$oldX<-data$x
+        .$newXY$oldY<-data$y
+      }
+      return(NULL)
+    }
 
-		trans_x <- function(x) {
-			setNewXY()
-			if(!swap_xy & any(.$newXY$oldX!=x))stop(simpleError('Mismatch between expected x and x in position'))
-			#beeswarm returns both x and y coordinates but it seems that x should not be changed.
-			#Just in case it does, we'll throw an error and investigate
-			if(swap_xy & any(.$newXY$oldX!=.$newXY$x))stop(simpleError('x position moved by beeswarm. Please make sure this is desired'))
-			.$newXY$x
-		}
-		trans_y <- function(y) {
-			setNewXY()
-			if(any(.$newXY$oldY!=y))stop(simpleError('Mismatch between expected y and y in position'))
-			#beeswarm returns both x and y coordinates but it seems that y should not be changed.
-			#Just in case it does, we'll throw an error and investigate
-			if(!swap_xy & any(.$newXY$oldY!=.$newXY$y))stop(simpleError('y position moved by beeswarm. Please make sure this is desired'))
-			.$newXY$y
-		}
-		 
-		transform_position(data, trans_x, trans_y)
-	}
+    trans_x <- function(x) {
+      setNewXY()
+      if(!swap_xy & any(.$newXY$oldX!=x))stop(simpleError('Mismatch between expected x and x in position'))
+      #beeswarm returns both x and y coordinates but it seems that x should not be changed.
+      #Just in case it does, we'll throw an error and investigate
+      if(swap_xy & any(.$newXY$oldX!=.$newXY$x))stop(simpleError('x position moved by beeswarm. Please make sure this is desired'))
+      .$newXY$x
+    }
+    trans_y <- function(y) {
+      setNewXY()
+      if(any(.$newXY$oldY!=y))stop(simpleError('Mismatch between expected y and y in position'))
+      #beeswarm returns both x and y coordinates but it seems that y should not be changed.
+      #Just in case it does, we'll throw an error and investigate
+      if(!swap_xy & any(.$newXY$oldY!=.$newXY$y))stop(simpleError('y position moved by beeswarm. Please make sure this is desired'))
+      .$newXY$y
+    }
+
+    transform_position(data, trans_x, trans_y)
+  }
 )
 
 PositionQuasirandom <- ggproto("PositionQuasirandom",ggplot2:::Position,required_aes=c('x','y'),
@@ -82,51 +82,51 @@ PositionQuasirandom <- ggproto("PositionQuasirandom",ggplot2:::Position,required
     list(width=self$width,varwidth=self$varwidth,bandwidth=self$bandwidth,nbins=self$nbins,method=self$method)
   },
   compute_layer= function(data,params,panel) {
-	 data <- remove_missing(data, vars = c("x","y"), name = "position_quasirandom")
-	 if (empty(data)) return(data.frame())
-	
-	 trans_x <- NULL
-	 trans_y <- NULL
+    data <- remove_missing(data, vars = c("x","y"), name = "position_quasirandom")
+    if (empty(data)) return(data.frame())
 
-	 # more unique entries in x than y suggests y (not x) is categorical	
-	 if(length(unique(data$y)) < length(unique(data$x))) {
-		 if (is.null(.$width)) .$width <- ggplot2::resolution(data$y, zero = FALSE) * 0.4
-	 } else {
-		 if (is.null(.$width)) .$width <- ggplot2::resolution(data$x, zero = FALSE) * 0.4
-	 }
-	 
- 
-	 if(.$width > 0) {
-		if(length(unique(data$y)) < length(unique(data$x))) {
-			trans_y <- function(y) {
-		  	  new_y <- vipor::offsetX( 
-				 data$x,
-			 	 y,
-			 	 width=.$width, 
-			 	 varwidth=.$varwidth, 
-				 adjust=.$bandwidth,
-				 method=.$method
-		  	  )
-		  
-		  	  new_y + y
-			}
-		} else {
-			trans_x <- function(x) {
-		  	  new_x <- vipor::offsetX( 
-				 data$y,
-			 	 x,
-			 	 width=.$width, 
-			 	 varwidth=.$varwidth, 
-				 adjust=.$bandwidth,
-				 method=.$method
-		  	  )
-		  
-		  	  new_x + x
-			}
-		}
-	 }
+    trans_x <- NULL
+    trans_y <- NULL
 
-	 transform_position(data, trans_x, trans_y)
+    # more unique entries in x than y suggests y (not x) is categorical	
+    if(length(unique(data$y)) < length(unique(data$x))) {
+      if (is.null(.$width)) .$width <- ggplot2::resolution(data$y, zero = FALSE) * 0.4
+    } else {
+      if (is.null(.$width)) .$width <- ggplot2::resolution(data$x, zero = FALSE) * 0.4
+    }
+
+
+    if(.$width > 0) {
+      if(length(unique(data$y)) < length(unique(data$x))) {
+        trans_y <- function(y) {
+          new_y <- vipor::offsetX( 
+            data$x,
+            y,
+            width=.$width, 
+            varwidth=.$varwidth, 
+            adjust=.$bandwidth,
+            method=.$method
+            )
+
+          new_y + y
+        }
+      } else {
+        trans_x <- function(x) {
+          new_x <- vipor::offsetX( 
+            data$y,
+            x,
+            width=.$width, 
+            varwidth=.$varwidth, 
+            adjust=.$bandwidth,
+            method=.$method
+            )
+
+          new_x + x
+        }
+      }
+    }
+
+    transform_position(data, trans_x, trans_y)
   }
 )
 
