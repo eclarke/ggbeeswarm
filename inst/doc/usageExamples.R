da4f8d8a7e99875eeff0fecdeaafd0bc0a6f2f1e
sherrillmix <sherrillmix@users.noreply.github.com>
2016-07-05 06:25:45 -0400

    Add options plots

--- inst/doc/usageExamples.R
+++ inst/doc/usageExamples.R
@@ -12,7 +12,7 @@ packageKeywords<-"visualization, display, one dimensional, grouped, groups, viol
 ###################################################
 ### code chunk number 2: ggPlot (eval = FALSE)
 ###################################################
-##   library(vipor)
+##   library(ggbeeswarm)
 ##   set.seed(12345)
 ##   n<-100
 ##   dat<-rnorm(n*2)
@@ -23,7 +23,7 @@ packageKeywords<-"visualization, display, one dimensional, grouped, groups, viol
 ###################################################
 ### code chunk number 3: showGgPlot
 ###################################################
-  library(vipor)
+  library(ggbeeswarm)
   set.seed(12345)
   n<-100
   dat<-rnorm(n*2)
@@ -47,13 +47,77 @@ packageKeywords<-"visualization, display, one dimensional, grouped, groups, viol
 ### code chunk number 6: ggFactors (eval = FALSE)
 ###################################################
 ##   labs2<-factor(labs,levels=c('b','a'))
-##   ggplot(mapping=aes(labs, dat)) + geom_quasirandom()
+##   ggplot(mapping=aes(labs2, dat)) + geom_quasirandom(aes(color=labs))
 
 
 ###################################################
 ### code chunk number 7: showGgFactors
 ###################################################
   labs2<-factor(labs,levels=c('b','a'))
-  ggplot(mapping=aes(labs, dat)) + geom_quasirandom()
+  ggplot(mapping=aes(labs2, dat)) + geom_quasirandom(aes(color=labs))
+
+
+###################################################
+### code chunk number 8: distAdjust (eval = FALSE)
+###################################################
+##   dat <- list(
+##     'Normal'=rnorm(50),
+##     'Dense normal'= rnorm(500),
+##     'Bimodal'=c(rnorm(100), rnorm(100,5)),
+##     'Extremes'=rcauchy(100)
+##   )
+##   labs<-rep(names(dat),sapply(dat,length),levels=names(dat))
+##   dat<-unlist(dat)
+##   ggplot(mapping=aes(labs, dat)) + geom_quasirandom(bandwidth=2)
+##   ggplot(mapping=aes(labs, dat)) + geom_quasirandom(bandwidth=.1)
+##   ggplot(mapping=aes(labs, dat)) + geom_quasirandom(width=.1)
+##   ggplot(mapping=aes(labs, dat)) + geom_quasirandom(nbins=100)
+
+
+###################################################
+### code chunk number 9: showDistAdjust
+###################################################
+  dat <- list(
+    'Normal'=rnorm(50),
+    'Dense normal'= rnorm(500),
+    'Bimodal'=c(rnorm(100), rnorm(100,5)),
+    'Extremes'=rcauchy(100)
+  )
+  labs<-rep(names(dat),sapply(dat,length),levels=names(dat))
+  dat<-unlist(dat)
+  ggplot(mapping=aes(labs, dat)) + geom_quasirandom(bandwidth=2)
+  ggplot(mapping=aes(labs, dat)) + geom_quasirandom(bandwidth=.1)
+  ggplot(mapping=aes(labs, dat)) + geom_quasirandom(width=.1)
+  ggplot(mapping=aes(labs, dat)) + geom_quasirandom(nbins=100)
+
+
+###################################################
+### code chunk number 10: varwidth (eval = FALSE)
+###################################################
+##   dat <- list(
+##     '10 points'=rnorm(10),
+##     '50 points'=rnorm(50,2),
+##     '200 points'=c(rnorm(400), rnorm(100,5)),
+##     '5000 points'= rnorm(5000,1)
+##   )
+##   labs<-rep(names(dat),sapply(dat,length))
+##   labs<-factor(labs,levels=unique(labs))
+##   dat<-unlist(dat)
+##   ggplot(mapping=aes(labs, dat)) + geom_quasirandom(varwidth=TRUE)
+
+
+###################################################
+### code chunk number 11: showVarwidth
+###################################################
+  dat <- list(
+    '10 points'=rnorm(10),
+    '50 points'=rnorm(50,2),
+    '200 points'=c(rnorm(400), rnorm(100,5)),
+    '5000 points'= rnorm(5000,1)
+  )
+  labs<-rep(names(dat),sapply(dat,length))
+  labs<-factor(labs,levels=unique(labs))
+  dat<-unlist(dat)
+  ggplot(mapping=aes(labs, dat)) + geom_quasirandom(varwidth=TRUE)
 
 
