34c24c3524a232c41f9788c3fac255bc510a6698
Charlotte Dawson <csdaw@outlook.com>
2021-06-24 09:44:46 +0100

    Adjust beeswarm code for readibility

--- R/geom-beeswarm.R
+++ R/geom-beeswarm.R
@@ -1,6 +1,7 @@
 #' Points, jittered to reduce overplotting using the beeswarm package
 #'
-#' The beeswarm geom is a convenient means to offset points within categories to reduce overplotting. Uses the beeswarm package
+#' The beeswarm geom is a convenient means to offset points within categories to
+#'  reduce overplotting. Uses the beeswarm package
 #'
 #' @section Aesthetics:
 #' \Sexpr[results=rd,stage=build]{ggplot2:::rd_aesthetics("geom", "point")}
@@ -31,16 +32,16 @@ geom_beeswarm <- function(
   mapping = NULL,
   data = NULL,
   priority = c("ascending", "descending", "density", "random", "none"),
-  groupOnX=NULL,
-  dodge.width=0,
-  beeswarmArgs=list(),
-  stat='identity',
+  groupOnX = NULL,
+  dodge.width = 0,
+  beeswarmArgs = list(),
+  stat = 'identity',
   na.rm = FALSE,
   show.legend = NA,
   inherit.aes = TRUE,
   ...
 ) {
-  position <- position_beeswarm(groupOnX=groupOnX,dodge.width=dodge.width)
+  position <- position_beeswarm(groupOnX = groupOnX, dodge.width = dodge.width)
   ggplot2::layer(
     data = data,
     mapping = mapping,
@@ -52,9 +53,9 @@ geom_beeswarm <- function(
     params = list(
       na.rm = na.rm,
       priority = priority,
-      groupOnX=groupOnX,
-      dodge.width=dodge.width,
-      beeswarmArgs=beeswarmArgs,
+      groupOnX = groupOnX,
+      dodge.width = dodge.width,
+      beeswarmArgs = beeswarmArgs,
       ...
     )
   )
@@ -63,27 +64,27 @@ geom_beeswarm <- function(
 #' geom object for use in geom_beeswarm
 #' @export
 GeomBeeswarm <- ggplot2::ggproto("GeomBeeswarm", ggplot2::GeomPoint,
-  draw_panel = function(data, panel_params, coord, priority = c("ascending", "descending", "density", "random", "none"), cex=1, groupOnX=NULL, dodge.width=0, beeswarmArgs=list(), na.rm = FALSE) {
-    if (is.character(data$shape)) {
-      data$shape <- ggplot2::translate_shape_string(data$shape)
-    }
-    coords <- coord$transform(data, panel_params)
-    grid::gTree(
-      coords=coords,
-      priority=priority,
-      groupOnX=groupOnX,
-      dodge.width=dodge.width,
-      beeswarmArgs=beeswarmArgs,
-      gp=grid::gpar(
-        col = alpha(coords$colour, coords$alpha),
-        fill = alpha(coords$fill, coords$alpha),
-        # Stroke is added around the outside of the point
-        fontsize = coords$size * .pt + coords$stroke * .stroke / 2,
-        lwd = coords$stroke * .stroke / 2
-      ),
-      cl = "beeswarm"
-    )
-  }
+                                 draw_panel = function(data, panel_params, coord, priority = c("ascending", "descending", "density", "random", "none"), cex=1, groupOnX=NULL, dodge.width=0, beeswarmArgs=list(), na.rm = FALSE) {
+                                   if (is.character(data$shape)) {
+                                     data$shape <- ggplot2::translate_shape_string(data$shape)
+                                   }
+                                   coords <- coord$transform(data, panel_params)
+                                   grid::gTree(
+                                     coords=coords,
+                                     priority=priority,
+                                     groupOnX=groupOnX,
+                                     dodge.width=dodge.width,
+                                     beeswarmArgs=beeswarmArgs,
+                                     gp=grid::gpar(
+                                       col = alpha(coords$colour, coords$alpha),
+                                       fill = alpha(coords$fill, coords$alpha),
+                                       # Stroke is added around the outside of the point
+                                       fontsize = coords$size * .pt + coords$stroke * .stroke / 2,
+                                       lwd = coords$stroke * .stroke / 2
+                                     ),
+                                     cl = "beeswarm"
+                                   )
+                                 }
 )
 
 #' grid::makeContent function for the grobTree of GeomBeeswarm objects
@@ -97,7 +98,7 @@ makeContent.beeswarm <- function(self) {
     grid::convertY(grid::grobHeight(grid::textGrob('o',gp=grid::gpar(cex=1))),'native')
   )
   coords<-offset_beeswarm(self$coords,priority=self$priority,cex=self$coords$size[1],groupOnX=self$groupOnX,dodge.width=self$dodge.width,beeswarmArgs=self$beeswarmArgs,oSize=oSize)
-
+  
   grid::pointsGrob(
     coords$x, coords$y,
     pch = coords$shape
--- R/position-beeswarm.R
+++ R/position-beeswarm.R
@@ -15,75 +15,96 @@
 #' @seealso \code{\link{geom_beeswarm}}, \code{\link{position_quasirandom}}, \code{\link[beeswarm]{swarmx}}
 
 
-offset_beeswarm= function(data,xRange=1,yRange=1,priority = c("ascending", "descending", "density", "random", "none"), cex=1, groupOnX=NULL, dodge.width=0, beeswarmArgs=list(),oSize=c(1/200,1/200)){
+offset_beeswarm= function(
+  data, 
+  xRange = 1, 
+  yRange = 1,
+  priority = c("ascending", "descending", "density", "random", "none"), 
+  cex = 1, 
+  groupOnX = NULL, 
+  dodge.width = 0, 
+  beeswarmArgs = list(),
+  oSize = c(1/200, 1/200)
+) {
   # Adjust function is used to calculate new positions (from ggplot2:::Position)
   data <- remove_missing(data, vars = c("x","y"), name = "position_beeswarm")
   if (nrow(data)==0) return(data.frame())
-
-  if(is.null(groupOnX)){
-    groupOnX<-TRUE
-    if(length(unique(data$y)) <= length(unique(data$x))) warning('The default behavior of beeswarm has changed in version 0.6.0. In versions <0.6.0, this plot would have been dodged on the y-axis.  In versions >=0.6.0, groupOnX=FALSE must be explicitly set to group on y-axis. Please set groupOnX=TRUE/FALSE to avoid this warning and ensure proper axis choice.')
+  
+  if (is.null(groupOnX)){
+    groupOnX <- TRUE
+    if (length(unique(data$y)) <= length(unique(data$x))) warning(paste(
+      'The default behavior of beeswarm has changed in version 0.6.0.',
+      'In versions <0.6.0, this plot would have been dodged on the y-axis.', 
+      'In versions >=0.6.0, groupOnX=FALSE must be explicitly set to group on y-axis.',
+      'Please set groupOnX=TRUE/FALSE to avoid this warning and ensure proper axis choice.'
+    ))
   }
-
+  
   #divisors are magic numbers to get a reasonable base spacing
   #note that the base beeswarm package is not 100% precise with differing plotting sizes resulting in variation in points overlap/spacing
-  xSize<-xRange*oSize[1]/.71
-  ySize<-yRange*oSize[2]/.92
-  offset<-stats::ave(
-    data[,ifelse(groupOnX,'y','x')],
-    data[,ifelse(groupOnX,'x','y')],
-    FUN=function(yy){
+  xSize<-xRange * oSize[1] / .71
+  ySize<-yRange * oSize[2] / .92
+  offset < -stats::ave(
+    data[, ifelse(groupOnX, 'y', 'x')],
+    data[, ifelse(groupOnX, 'x', 'y')],
+    FUN = function(yy){
       if (length(yy) == 1) return(0)
       else do.call(beeswarm::swarmx,c(list(
         0,
         yy,
-        cex=1,
-        priority=priority,
-        xsize=ifelse(groupOnX,xSize,ySize),
-        ysize=ifelse(groupOnX,ySize,xSize)
-      ),beeswarmArgs)
-    )$x
+        cex = 1,
+        priority = priority,
+        xsize = ifelse(groupOnX, xSize, ySize),
+        ysize = ifelse(groupOnX, ySize, xSize)
+      ), beeswarmArgs)
+      )$x
     }
   )
-  data[,ifelse(groupOnX,'x','y')]<-data[,ifelse(groupOnX,'x','y')]+offset
-
+  data[, ifelse(groupOnX, 'x', 'y')] <- data[, ifelse(groupOnX, 'x', 'y')] + offset
+  
   return(data)
 }
-    
-position_beeswarm <- function (groupOnX=NULL,dodge.width=0){
-  ggplot2::ggproto(NULL,PositionBeeswarm,groupOnX=groupOnX,dodge.width=dodge.width)
+
+position_beeswarm <- function (groupOnX = NULL, dodge.width = 0) {
+  ggplot2::ggproto(NULL, PositionBeeswarm, groupOnX = groupOnX, dodge.width = dodge.width)
 }
 
-PositionBeeswarm <- ggplot2::ggproto("PositionBeeswarm",ggplot2:::Position,required_aes=c('x','y'),
-  setup_params=function(self,data){
-    list(groupOnX=self$groupOnX,dodge.width=self$dodge.width)
-  },
-  compute_panel= function(data,params,scales){
-    data <- remove_missing(data, vars = c("x","y"), name = "position_quasirandom")
-    if (nrow(data)==0) return(data.frame())
-
-    if(is.null(params$groupOnX)){
-      params$groupOnX<-TRUE
-      if(length(unique(data$y)) <= length(unique(data$x))) warning('The default behavior of beeswarm has changed in version 0.6.0. In versions <0.6.0, this plot would have been dodged on the y-axis.  In versions >=0.6.0, groupOnX=FALSE must be explicitly set to group on y-axis. Please set groupOnX=TRUE/FALSE to avoid this warning and ensure proper axis choice.')
-    }
-
-    # dodge
-    if(!params$groupOnX){
-      data[,c('x','y')]<-data[,c('y','x')]
-      origCols<-colnames(data)
-    }
-    data <- ggplot2:::collide(
-      data,
-      params$dodge.width,
-      "position_dodge",
-      ggplot2:::pos_dodge,
-      check.width = FALSE
-    )
-    if(!params$groupOnX){
-      data[,c('x','y')]<-data[,c('y','x')]
-      #remove x/y min/max created by collide
-      data<-data[,origCols]
-    }
-    return(data)
-  }
+PositionBeeswarm <- ggplot2::ggproto("PositionBeeswarm", ggplot2:::Position, 
+                                     required_aes = c('x','y'),
+                                     setup_params = function(self, data) {
+                                       list(groupOnX = self$groupOnX, dodge.width = self$dodge.width)
+                                     },
+                                     compute_panel = function(data, params, scales) {
+                                       data <- remove_missing(data, vars = c("x", "y"), name = "position_quasirandom")
+                                       if (nrow(data) == 0) return(data.frame())
+                                       
+                                       if (is.null(params$groupOnX)){
+                                         params$groupOnX <- TRUE
+                                         if (length(unique(data$y)) <= length(unique(data$x))) warning(paste(
+                                           'The default behavior of beeswarm has changed in version 0.6.0.',
+                                           'In versions <0.6.0, this plot would have been dodged on the y-axis.',
+                                           'In versions >=0.6.0, groupOnX=FALSE must be explicitly set to group on y-axis.',
+                                           'Please set groupOnX=TRUE/FALSE to avoid this warning and ensure proper axis choice.'
+                                         ))
+                                       }
+                                       
+                                       # dodge
+                                       if (!params$groupOnX){
+                                         data[, c('x', 'y')] <- data[, c('y', 'x')]
+                                         origCols <- colnames(data)
+                                       }
+                                       data <- ggplot2:::collide(
+                                         data,
+                                         params$dodge.width,
+                                         "position_dodge",
+                                         ggplot2:::pos_dodge,
+                                         check.width = FALSE
+                                       )
+                                       if(!params$groupOnX){
+                                         data[, c('x', 'y')] <- data[, c('y', 'x')]
+                                         #remove x/y min/max created by collide
+                                         data <- data[, origCols]
+                                       }
+                                       return(data)
+                                     }
 )
